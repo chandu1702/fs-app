5546c7ec48d21e3ee75a85cc993a0f05a4a14320
Ramachandra Pallikonda <ramachandra.10660349@ltimindtree.com>
2025-02-20 10:41:39 +0530

    Initial payments simulator module

---
+++ payments-simulator/src/main/java/com/payments/toolbox/simulator/PaymentsSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.payments.toolbox.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsSimulatorApplication.class, args);
+	}
+
+}
---
+++ payments-simulator/src/test/java/com/payments/toolbox/simulator/PaymentsSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments.toolbox.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

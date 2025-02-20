b8160925ecea01929f6a4eff5ddbbecca7023926
Ramachandra Pallikonda <ramachandra.10660349@ltimindtree.com>
2025-02-20 12:43:12 +0530

    updated props for h2 db

--- payments-simulator/src/main/java/com/payments/toolbox/simulator/PaymentsSimulatorApplication.java
+++ payments-simulator/src/main/java/com/payments/toolbox/simulator/PaymentsSimulatorApplication.java
@@ -3,11 +3,24 @@ package com.payments.toolbox.simulator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class for the Payments Simulator Application.
+ * This class is responsible for bootstrapping the Spring Boot application.
+ */
 @SpringBootApplication
 public class PaymentsSimulatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentsSimulatorApplication.class, args);
-	}
+    /**
+     * The main method which serves as the entry point for the Spring Boot application.
+     *
+     * @param args Command line arguments passed to the application.
+     */
+    public static void main(String[] args) {
+        // Print a greeting message to the console
+        System.out.println("Hello");
+
+        // Launch the Spring Boot application
+        SpringApplication.run(PaymentsSimulatorApplication.class, args);
+    }
 
 }
